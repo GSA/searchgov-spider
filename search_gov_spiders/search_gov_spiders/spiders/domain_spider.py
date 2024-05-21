9c04d6c01c7df865f3e9ed6baa5ddb9e68f533c9
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-05-21 10:07:18 -0400

    Moved mock and test directories
    Moved mock and test directories one level up

--- search_gov_spiders/search_gov_spiders/spiders/domain_spider.py
+++ search_gov_spiders/search_gov_spiders/spiders/domain_spider.py
@@ -1,14 +1,14 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-starting_urls = '../../utility-scripts/startingUrls.txt'
+starting_urls = "../utility_files/startingUrls.txt"
 
 start_urls_list = []
 with open(starting_urls) as file:
     while line := file.readline():
         start_urls_list.append(line.rstrip())
 
-domains = '../../utility-scripts/domains.txt'
+domains = "../utility_files/domains.txt"
 domains_list = []
 with open(domains) as file:
     while line := file.readline():
@@ -18,6 +18,7 @@ with open(domains) as file:
 # scrapy command for crawling domain/site
 # scrapy crawl domain_spider -a domain=desired_domain -a urls=desired_url
 # ex: scrapy crawl domain_spider -a domain=travel.dod.mil -a urls=https://travel.dod.mil
+
 
 class DomainSpider(CrawlSpider):
     # name = "travelDodMil"
@@ -29,41 +30,50 @@ class DomainSpider(CrawlSpider):
         self.start_urls = [urls] if urls else start_urls_list
 
     # file type exclusions
-    rules = (Rule(LinkExtractor(allow=(),
-                                deny=[
-                                    "calendar",
-                                    "location-contact",
-                                    "DTMO-Site-Map/FileId/"
-                                    # "\*redirect"
-                                ],
-                                deny_extensions=[
-                                    'js',
-                                    'xml',
-                                    'gif',
-                                    'wmv',
-                                    'wav',
-                                    'ibooks',
-                                    'zip',
-                                    'css',
-                                    'mp3',
-                                    'mp4',
-                                    'cfm',
-                                    'jpg',
-                                    'jpeg',
-                                    'png',
-                                    'exe',
-                                    'svg',
-                                    'ppt',
-                                    'pptx',
-                                    'ics',
-                                    'nc',
-                                    'tif',
-                                    'prj',
-                                    'tar',
-                                    'tar.gz',
-                                    'rss',
-                                    'sfx'
-                                ], unique=True), callback="parse_item", follow=True),)
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=(),
+                deny=[
+                    "calendar",
+                    "location-contact",
+                    "DTMO-Site-Map/FileId/",
+                    # "\*redirect"
+                ],
+                deny_extensions=[
+                    "js",
+                    "xml",
+                    "gif",
+                    "wmv",
+                    "wav",
+                    "ibooks",
+                    "zip",
+                    "css",
+                    "mp3",
+                    "mp4",
+                    "cfm",
+                    "jpg",
+                    "jpeg",
+                    "png",
+                    "exe",
+                    "svg",
+                    "ppt",
+                    "pptx",
+                    "ics",
+                    "nc",
+                    "tif",
+                    "prj",
+                    "tar",
+                    "tar.gz",
+                    "rss",
+                    "sfx",
+                ],
+                unique=True,
+            ),
+            callback="parse_item",
+            follow=True,
+        ),
+    )
 
     @staticmethod
     def parse_item(response):
@@ -74,6 +84,4 @@ class DomainSpider(CrawlSpider):
         @returns requests 0 0
         @scrapes Status Link
         """
-        yield {
-            "Link": response.url
-        }
+        yield {"Link": response.url}
