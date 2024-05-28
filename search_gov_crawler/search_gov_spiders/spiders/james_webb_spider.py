a27962894ef4175c56d867cae8d76b7940e2c7c1
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-05-28 10:56:37 -0400

    James Webb Spider

--- search_gov_crawler/search_gov_spiders/spiders/james_webb_spider.py
+++ search_gov_crawler/search_gov_spiders/spiders/james_webb_spider.py
@@ -3,14 +3,23 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
-class JamesWebbSpiderSpider(CrawlSpider):
+class JamesWebbSpider(CrawlSpider):
     name = "james_webb_spider"
-    allowed_domains = ["jwst.nasa.gov"]
+    allowed_domains = ["nasa.gov"]
     start_urls = ["https://www.jwst.nasa.gov/"]
 
-    rules = (Rule(LinkExtractor(allow=(), deny=["calendar", "location-contact"], unique=True), callback="parse_item", follow=True),)
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=(),
+                deny=["calendar", "location-contact"],
+                unique=True,
+            ),
+            callback="parse_item",
+            follow=True,
+        ),
+    )
 
+    # only including "url": response.url and not another response argument throws error
     def parse_item(self, response):
-        yield {
-            "Link": response.url
-        }
+        yield {"Link": response.url, "h1": response.xpath("//h1/text()").get()}
