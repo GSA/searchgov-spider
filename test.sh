e6d4c3bfe1d98f491f84a1fe602c43cc4127736f
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2024-08-06 12:34:59 -0400

    Fixed code runner

--- test.sh
+++ test.sh
@@ -2,9 +2,18 @@
 
 if [ ! -f "/usr/local/bin/codeclimate" ]
 then
-	curl -L https://github.com/codeclimate/codeclimate/archive/master.tar.gz | tar xvz -C /home/$USER/
-	(cd /home/$USER/codeclimate-* && sudo make install)
+	curl -L https://github.com/codeclimate/codeclimate/archive/master.tar.gz | tar xvz -C $HOME/
+	(cd $HOME/codeclimate-* && sudo make install)
+	echo "sudo docker run \
+--interactive --tty --rm \
+--env CODECLIMATE_CODE="$PWD" \
+--volume "$PWD":/code \
+--volume /var/run/docker.sock:/var/run/docker.sock \
+--volume /tmp/cc:/tmp/cc \
+codeclimate/codeclimate analyze" > "codeclimate.sh"
 fi
+
+chmod 755  /usr/local/bin/codeclimate
 
 codeclimate engines:install
 codeclimate analyze
