b0c1366d3c70ae4ec51fdce8e7db555fea43ae68
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-08-07 15:43:09 -0400

    SRCH-5427-Implement-ScrapyD
    setup.py needs to be updated for scrapyd

--- search_gov_crawler/setup.py
+++ search_gov_crawler/setup.py
@@ -1,12 +1,10 @@
 # Automatically created by: scrapyd-deploy
 
 from setuptools import setup, find_packages
-from scrapyd_api import ScrapydAPI
 
 setup(
-    name="project",
+    name="search_gov_spiders",
     version="1.0",
     packages=find_packages(),
     entry_points={"scrapy": ["settings = search_gov_spiders.settings"]},
-    scrapyd=ScrapydAPI("http://localhost:6800"),
 )
