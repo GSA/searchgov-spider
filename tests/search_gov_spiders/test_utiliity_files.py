6be194ff79809f3bf935b4083cd8df4e7453dd68
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2024-08-17 20:18:06 -0400

    SRCH-5430 update tests for plist import

--- tests/search_gov_spiders/test_utiliity_files.py
+++ tests/search_gov_spiders/test_utiliity_files.py
@@ -1,72 +1,82 @@
 import json
-from collections import namedtuple
+import plistlib
+import tempfile
+from datetime import datetime
 from pathlib import Path
 
 import pytest
 
-from search_gov_crawler.search_gov_spiders.helpers import domain_spider as helpers
-
-
-@pytest.mark.parametrize(
-    ("content_type_header", "result"),
-    [("text/html", True), ("application/msword.more.and.more", True), ("Something/Else", False)],
-    ids=["good", "regex", "bad"],
+from search_gov_crawler.search_gov_spiders.utility_files.import_plist import (
+    convert_plist_to_json,
+    create_allowed_domain,
 )
-def test_is_valid_content_type(content_type_header, result):
-    assert helpers.is_valid_content_type(content_type_header) is result
 
 
-def test_get_crawl_sites_default():
-    assert len(helpers.get_crawl_sites()) == 66
+ALLOWED_DOMAIN_TEST_CASES = [
+    ("https://www.example.com", "example.com"),
+    ("https://example.com", "example.com"),
+    ("https://www2.example.com", "example.com"),
+    ("https://www7.example.com", "example.com"),
+    ("https://www2.example.com/another/page", "example.com"),
+]
 
 
-@pytest.fixture(name="crawl_sites_test_file")
-def fixture_crawl_sites_test_file():
-    return Path(__file__).parent / "crawl-sites-test.json"
+@pytest.mark.parametrize(("input_url", "expected_result"), ALLOWED_DOMAIN_TEST_CASES)
+def test_create_allowed_domain(input_url, expected_result):
+    assert create_allowed_domain(input_url) == expected_result
 
 
-@pytest.fixture(name="crawl_sites_test_file_json")
-def fixture_crawl_sites_test_file_json(crawl_sites_test_file):
-    return json.loads(crawl_sites_test_file.resolve().read_text())
+def test_convert_plist_to_json(monkeypatch):
+    def mock_read_text(*args, **kwargs):
+        return "some text"
 
+    def mock_plist_loads(*args, **kwargs):
+        return [
+            {
+                "dateStamp": datetime(2024, 1, 1, 12, 12, 12),
+                "startingUrl": "https://www.example.com/1",
+                "runJS": True,
+                "AnotherField": 100,
+            },
+            {
+                "dateStamp": datetime(2024, 2, 1, 12, 12, 12),
+                "startingUrl": "https://www.example.com/2",
+                "runJS": False,
+                "AnotherField": 200,
+            },
+            {
+                "dateStamp": datetime(2024, 3, 1, 12, 12, 12),
+                "startingUrl": "https://www.example.com/3",
+                "runJS": True,
+                "AnotherField": 300,
+            },
+        ]
 
-def test_get_crawl_sites_test_file(crawl_sites_test_file):
-    assert len(helpers.get_crawl_sites(str(crawl_sites_test_file.resolve()))) == 3
+    monkeypatch.setattr(Path, "exists", lambda x: True)
+    monkeypatch.setattr(plistlib, "loads", mock_plist_loads)
 
+    with tempfile.TemporaryDirectory() as temp_dir:
+        monkeypatch.setattr(Path, "parent", Path(temp_dir))
 
-@pytest.mark.parametrize(("handle_javascript", "results"), [(True, 2), (False, 1)])
-def test_default_starting_urls(monkeypatch, crawl_sites_test_file_json, handle_javascript, results):
-    def mock_get_crawl_sites(*args, **kwargs):
-        return crawl_sites_test_file_json
+        with monkeypatch.context() as m:
+            m.setattr(Path, "read_text", mock_read_text)
+            convert_plist_to_json(input_file="input_file.plist", output_file="crawl-sites.json", write_full_output=True)
 
-    monkeypatch.setattr(
-        "search_gov_crawler.search_gov_spiders.helpers.domain_spider.get_crawl_sites", mock_get_crawl_sites
-    )
+        crawl_output_records = json.loads(
+            Path(temp_dir).joinpath("crawl-sites.json").resolve().read_text(encoding="UTF")
+        )
+        full_output_records = json.loads(Path(temp_dir).joinpath("input_file.json").resolve().read_text(encoding="UTF"))
 
-    starting_urls = helpers.default_starting_urls(handle_javascript)
-    assert len(starting_urls) == results
-
-
-@pytest.mark.parametrize(("handle_javascript", "results"), [(True, 2), (False, 1)])
-def test_default_allowed_domains(monkeypatch, crawl_sites_test_file_json, handle_javascript, results):
-    def mock_get_crawl_sites(*args, **kwargs):
-        return crawl_sites_test_file_json
-
-    monkeypatch.setattr(
-        "search_gov_crawler.search_gov_spiders.helpers.domain_spider.get_crawl_sites", mock_get_crawl_sites
-    )
-
-    starting_urls = helpers.default_allowed_domains(handle_javascript)
-    assert len(starting_urls) == results
-
-
-Request = namedtuple("Request", ["resource_type", "should_abort"])
-
-
-@pytest.fixture(name="request_with_resource_type", params=[("jpeg", True), ("html", False)], ids=["Valid", "Invalid"])
-def fixture_request_with_resource_type(request) -> Request:
-    return Request(*request.param)
-
-
-def test_should_abort_request(request_with_resource_type):
-    assert helpers.should_abort_request(request_with_resource_type) == request_with_resource_type.should_abort
+        assert len(crawl_output_records) == 3
+        assert crawl_output_records[0] == {
+            "allowed_domains": "example.com",
+            "handle_javascript": True,
+            "starting_urls": "https://www.example.com/1",
+        }
+        assert len(full_output_records) == 3
+        assert full_output_records[0] == {
+            "dateStamp": "2024-01-01T12:12:12",
+            "startingUrl": "https://www.example.com/1",
+            "runJS": True,
+            "AnotherField": 100,
+        }
