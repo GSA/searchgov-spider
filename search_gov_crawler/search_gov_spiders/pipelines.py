d42b78acf701d00a1ff23ed3281b4fbe81d24f08
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2024-08-23 10:25:36 -0400

    SRCH-5430 update item pipeline to use single output dir and add test coverage for item pipeline

--- search_gov_crawler/search_gov_spiders/pipelines.py
+++ search_gov_crawler/search_gov_spiders/pipelines.py
@@ -4,6 +4,8 @@ See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 """
 
 import os
+from pathlib import Path
+
 from scrapy.exceptions import DropItem
 
 
@@ -13,29 +15,32 @@ class SearchGovSpidersPipeline:
     to output file with a max size of 3.9MB
     """
 
-    current_file_size = 0
-    file_number = 1
-    base_path_name = "output/all-links.csv"
-    max_file_size = 3900
-    PAGINATE = True
+    def __init__(self, *_args, **_kwargs):
+        self.current_file_size = 0
+        self.file_number = 1
+        self.parent_file_path = Path(__file__).parent.parent.resolve()
+        self.base_path_name = str(self.parent_file_path / "output/all-links.csv")
+        self.short_file = open(self.base_path_name, "w", encoding="utf-8")
+        self.max_file_size = 3900
+        self.paginate = True
 
     def process_item(self, item, _spider):
         """Checks that the file is not at max size.
         Adds it to the file if less, or creates a new file if too large."""
-        short_file = open(self.base_path_name, "w", encoding="utf-8")
+
         line = item["url"]
         self.current_file_size += 1
         next_file_size = self.current_file_size + len(line)
-        if self.PAGINATE and next_file_size > self.max_file_size:
-            short_file.close()
-            new_name = "output/all-links" + str(self.file_number) + ".csv"
+        if self.paginate and next_file_size > self.max_file_size:
+            self.short_file.close()
+            new_name = str(self.parent_file_path / f"output/all-links{self.file_number}.csv")
             os.rename(self.base_path_name, new_name)
             self.file_number = self.file_number + 1
-            short_file = open(self.base_path_name, "w", encoding="utf-8")
+            self.short_file = open(self.base_path_name, "w", encoding="utf-8")
             self.current_file_size = 0
 
-        short_file.write(line)
-        short_file.write("\n")
+        self.short_file.write(line)
+        self.short_file.write("\n")
         self.current_file_size = self.current_file_size + len(line)
 
         return item
--- tests/search_gov_spiders/test_full_crawl.py
+++ tests/search_gov_spiders/test_full_crawl.py
@@ -62,21 +62,35 @@ FULL_CRAWL_TEST_CASES = [
 ]
 
 
-@pytest.mark.parametrize(("spider", "use_item_pipeline", "crawl_kwargs", "expected_results"), FULL_CRAWL_TEST_CASES)
-def test_full_crawl(mock_scrapy_settings, spider, use_item_pipeline, crawl_kwargs, expected_results):
-    if use_item_pipeline:
-        mock_scrapy_settings.set(
-            "ITEM_PIPELINES",
-            {
-                f"search_gov_crawler.{k}": v
-                for k, v in dict(mock_scrapy_settings.get("ITEM_PIPELINES")).items()
-                if k != "search_gov_spiders.pipelines.SearchGovSpidersPipeline"
-            },
-        )
-    else:
-        mock_scrapy_settings.delete("ITEM_PIPELINES")
+@pytest.mark.parametrize(("spider", "use_dedup", "crawl_kwargs", "expected_results"), FULL_CRAWL_TEST_CASES)
+def test_full_crawl(mock_scrapy_settings, monkeypatch, spider, use_dedup, crawl_kwargs, expected_results):
+    # only use dedup pipeline for js test, otherwise dupes are not cleared between runs
+    mock_scrapy_settings.set(
+        "ITEM_PIPELINES",
+        {
+            f"search_gov_crawler.{k}": v
+            for k, v in dict(mock_scrapy_settings.get("ITEM_PIPELINES")).items()
+            if (k == "search_gov_spiders.pipelines.SearchGovSpidersPipeline" or use_dedup)
+        },
+    )
 
     with tempfile.NamedTemporaryFile(suffix=".json") as output_file:
+        temp_dir = Path(str(output_file.name)).parent
+        temp_dir.joinpath("output").mkdir(exist_ok=True)
+
+        def mock_init(pipeline_cls, *_args, temp_dir=temp_dir, **_kwargs):
+            pipeline_cls.current_file_size = 0
+            pipeline_cls.file_number = 1
+            pipeline_cls.parent_file_path = temp_dir
+            pipeline_cls.base_path_name = str(pipeline_cls.parent_file_path / "output/all-links.csv")
+            pipeline_cls.short_file = open(pipeline_cls.base_path_name, "w", encoding="utf-8")
+            pipeline_cls.max_file_size = 3900
+            pipeline_cls.paginate = True
+
+        monkeypatch.setattr(
+            "search_gov_crawler.search_gov_spiders.pipelines.SearchGovSpidersPipeline.__init__", mock_init
+        )
+
         mock_scrapy_settings.set("FEEDS", {output_file.name: {"format": "json"}})
 
         process = CrawlerProcess(mock_scrapy_settings)
@@ -86,4 +100,10 @@ def test_full_crawl(mock_scrapy_settings, spider, use_item_pipeline, crawl_kwarg
         with open(output_file.name, encoding="UTF") as f:
             links = json.load(f)
 
+        split_files = list(temp_dir.glob("all-links*.csv"))
+
+        # verify total links match expected
         assert len(links) == expected_results
+
+        # verify split files exist and are under max file size
+        assert all(split_file.stat().st_size < 3900 for split_file in split_files)
