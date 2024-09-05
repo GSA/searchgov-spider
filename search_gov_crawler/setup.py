0632f68cb0cd6339e193216270aba7df71254d06
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2024-09-05 13:30:07 -0400

    SRCH-5475 remove crawler init, main , and pip settings because they were interfering with scrapyd-deploy

--- search_gov_crawler/setup.py
+++ search_gov_crawler/setup.py
@@ -6,7 +6,7 @@ from scrapyd_api import ScrapydAPI
 setup(
     name="search_gov_spiders",
     version="1.0",
-    packages=find_packages(),
-    entry_points={"scrapy": ["settings = search_gov_spiders.settings"]},
+    packages=find_packages(exclude=["search_gov_logparser", "search_gov_scrapyd", "search_gov_scrapydweb"]),
+    entry_points={"scrapy": ["settings = search_gov_crawler.search_gov_spiders.settings"]},
     scrapyd=ScrapydAPI("http://localhost:6800"),
 )
