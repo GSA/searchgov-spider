44034fc2141e55f706cc3bcf2241fc7dcbf3ba6d
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2024-09-30 12:27:20 -0400

    Fixed cron jobs

--- cicd-scripts/app_install.sh
+++ cicd-scripts/app_install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# CICD scripts can only runas 'search' user on AWS
+if [ "$(whoami)" == "search" ]; then
+  echo "Executing cicd scripts as 'search' user"
+else
+  echo "This script must be executed as 'search' user"
+  return 1
+fi
+
+sudo apt install acl -y
+
+# Required to give all app_* bash scripts read/write permissions to self and parent.
+# Give current directory and all its files rw permissions
+sudo chmod -R 755 .
+# All new files/directories will inherit rwx (required when installing and using sqllite)
+sudo setfacl -Rdm g:dgsearch:rwx .
 
 # A hack to get the environment running without ansible local env variables
 # This block of code will eventually be removed once ansible task is completed
@@ -57,15 +72,12 @@ fi
 sudo /usr/local/bin/python${SPIDER_PYTHON_VERSION} -m pip install --upgrade pip
 sudo /usr/local/bin/python${SPIDER_PYTHON_VERSION} -m pip install virtualenv
 
-# Navigate to the spider directory
-cd /home/ec2-user/spider
-
 # Create a virtual environment using Python
 echo "Creating pytho${SPIDER_PYTHON_VERSION} virtual environment..."
-/usr/local/bin/python${SPIDER_PYTHON_VERSION} -m venv /home/ec2-user/spider/venv
+/usr/local/bin/python${SPIDER_PYTHON_VERSION} -m venv ./venv
 
 # Activate the virtual environment
-source /home/ec2-user/spider/venv/bin/activate
+source ./venv/bin/activate
 
 # Install all spider dependencies
 echo "Installing dependencies..."
@@ -73,3 +85,34 @@ pip install --upgrade pip
 pip install --upgrade --force-reinstall -r ./search_gov_crawler/requirements.txt
 
 echo "Dependencies installed."
+
+
+# Remove any outstanding app_start.sh reboot cronjobs
+echo "Removing any app_start.sh reboot cron jobs..."
+crontab -l > cron_backup.bak
+
+# Remove lines containing 'app_start.sh' and update crontab
+crontab -l | grep -v 'app_start.sh' > cron_backup_filtered
+
+# Check if there are changes
+if cmp -s cron_backup_filtered cron_backup.bak; then
+  echo "No cron jobs with 'app_start.sh' found."
+else
+  crontab cron_backup_filtered
+  echo "Cron jobs containing 'app_start.sh' have been removed."
+fi
+
+# Clean up temporary files
+rm cron_backup_filtered cron_backup.bak
+
+# Add cron job to run the app back up on ec2 restart
+echo "Adding app_start.sh reboot cron job..."
+sudo chmod +x ./cicd-scripts/app_start.sh
+
+# Define the new cron job
+new_cron="@reboot at now + 1 min -f ${pwd}/cicd-scripts/app_start.sh"
+
+# Add the new cron job to the crontab if it's not already present
+(crontab -l | grep -v "$new_cron" ; echo "$new_cron") | crontab -
+
+echo "Cron job added: $new_cron"
