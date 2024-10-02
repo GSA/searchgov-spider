b4354faeac3b0766b8938edb6db0bbecd8de11be
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2024-10-02 07:04:53 -0400

    SRCH-5548 add ability to filter paths as needed.

--- tests/search_gov_spiders/test_helpers.py
+++ tests/search_gov_spiders/test_helpers.py
@@ -18,7 +18,7 @@ def test_is_valid_content_type(content_type_header, result):
 
 
 def test_get_crawl_sites_default():
-    assert len(helpers.get_crawl_sites()) == 46
+    assert len(helpers.get_crawl_sites()) == 43
 
 
 @pytest.fixture(name="crawl_sites_test_file")
@@ -32,10 +32,10 @@ def fixture_crawl_sites_test_file_json(crawl_sites_test_file):
 
 
 def test_get_crawl_sites_test_file(crawl_sites_test_file):
-    assert len(helpers.get_crawl_sites(str(crawl_sites_test_file.resolve()))) == 3
+    assert len(helpers.get_crawl_sites(str(crawl_sites_test_file.resolve()))) == 4
 
 
-@pytest.mark.parametrize(("handle_javascript", "results"), [(True, 2), (False, 1)])
+@pytest.mark.parametrize(("handle_javascript", "results"), [(True, 2), (False, 2)])
 def test_default_starting_urls(monkeypatch, crawl_sites_test_file_json, handle_javascript, results):
     def mock_get_crawl_sites(*_args, **_kwargs):
         return crawl_sites_test_file_json
@@ -48,7 +48,7 @@ def test_default_starting_urls(monkeypatch, crawl_sites_test_file_json, handle_j
     assert len(starting_urls) == results
 
 
-@pytest.mark.parametrize(("handle_javascript", "results"), [(True, 2), (False, 1)])
+@pytest.mark.parametrize(("handle_javascript", "results"), [(True, 2), (False, 2)])
 def test_default_allowed_domains(monkeypatch, crawl_sites_test_file_json, handle_javascript, results):
     def mock_get_crawl_sites(*_args, **_kwargs):
         return crawl_sites_test_file_json
@@ -57,8 +57,27 @@ def test_default_allowed_domains(monkeypatch, crawl_sites_test_file_json, handle
         "search_gov_crawler.search_gov_spiders.helpers.domain_spider.get_crawl_sites", mock_get_crawl_sites
     )
 
-    starting_urls = helpers.default_allowed_domains(handle_javascript)
-    assert len(starting_urls) == results
+    allowed_domains = helpers.default_allowed_domains(handle_javascript=handle_javascript)
+    assert len(allowed_domains) == results
+
+
+@pytest.mark.parametrize(
+    ("remove_paths", "results"),
+    [
+        (False, ["quotes.toscrape.com", "quotes.toscrape.com/tag/"]),
+        (True, ["quotes.toscrape.com", "quotes.toscrape.com"]),
+    ],
+)
+def test_default_allowed_domains_remove_paths(monkeypatch, crawl_sites_test_file_json, remove_paths, results):
+    def mock_get_crawl_sites(*_args, **_kwargs):
+        return crawl_sites_test_file_json
+
+    monkeypatch.setattr(
+        "search_gov_crawler.search_gov_spiders.helpers.domain_spider.get_crawl_sites", mock_get_crawl_sites
+    )
+
+    allowed_domains = helpers.default_allowed_domains(handle_javascript=False, remove_paths=remove_paths)
+    assert allowed_domains == results
 
 
 Request = namedtuple("Request", ["resource_type", "should_abort"])
@@ -71,3 +90,7 @@ def fixture_request_with_resource_type(request) -> Request:
 
 def test_should_abort_request(request_with_resource_type):
     assert should_abort_request(request_with_resource_type) == request_with_resource_type.should_abort
+
+
+def test_split_allowed_domains():
+    assert helpers.split_allowed_domains("test.com,example.com/home") == ["test.com", "example.com"]
