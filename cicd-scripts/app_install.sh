ef4e3f5724acb22066819aaa332db69ca0c18590
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2024-10-07 15:56:28 -0400

    ...

--- cicd-scripts/app_install.sh
+++ cicd-scripts/app_install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # CICD scripts can only runas 'search' user on AWS
-if [ "$(whoami)" == "search" ]; then
+if [ "$(whoami)" = "search" ]; then
   echo "Executing cicd scripts as 'search' user"
 else
   echo "This script must be executed as 'search' user"
-  return 1
+  exit 1
 fi
 
+SPIDER_PYTHON_VERSION=3.12
+
+# Update and upgrade the system without prompting for confirmation
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt install acl -y
 
 # Required to give all app_* bash scripts read/write permissions to self and parent.
@@ -16,29 +21,9 @@ sudo chmod -R 755 .
 # All new files/directories will inherit rwx (required when installing and using sqllite)
 sudo setfacl -Rdm g:dgsearch:rwx .
 
-# A hack to get the environment running without ansible local env variables
-# This block of code will eventually be removed once ansible task is completed
-SPIDER_PYTHON_VERSION=3.12
-SPIDER_STAGING_URLS_API=https://staging.search.usa.gov/urls
-spider_local_path=/etc/profile.d/spider_local.sh
-
-# Writing environment variables to the profile file
-echo "
-export SPIDER_PYTHON_VERSION=${SPIDER_PYTHON_VERSION}
-export SPIDER_STAGING_URLS_API=${SPIDER_STAGING_URLS_API}
-" | tee "$spider_local_path" > /dev/null
-
-# Source the script to update the current shell's environment
-source "$spider_local_path"
-### TODO: Remove the above code block after ansible is fully implmented
-
-
-# Update and upgrade the system without prompting for confirmation
-sudo apt-get update -y
-sudo apt-get upgrade -y
 
 # Install necessary system dependencies
-sudo apt-get install -y python-setuptools python-pip
+sudo apt-get install -y python-setuptools
 
 install_python() {
     echo "Installing ${SPIDER_PYTHON_VERSION}"
@@ -73,8 +58,8 @@ sudo /usr/local/bin/python${SPIDER_PYTHON_VERSION} -m pip install --upgrade pip
 sudo /usr/local/bin/python${SPIDER_PYTHON_VERSION} -m pip install virtualenv
 
 # Create a virtual environment using Python
-echo "Creating pytho${SPIDER_PYTHON_VERSION} virtual environment..."
-/usr/local/bin/python${SPIDER_PYTHON_VERSION} -m venv ./venv
+echo "Creating python${SPIDER_PYTHON_VERSION} virtual environment..."
+sudo /usr/local/bin/python${SPIDER_PYTHON_VERSION} -m venv ./venv
 
 # Activate the virtual environment
 source ./venv/bin/activate
@@ -82,7 +67,7 @@ source ./venv/bin/activate
 # Install all spider dependencies
 echo "Installing dependencies..."
 pip install --upgrade pip
-pip install --upgrade --force-reinstall -r ./search_gov_crawler/requirements.txt
+sudo pip install --upgrade --force-reinstall -r ./search_gov_crawler/requirements.txt
 
 echo "Dependencies installed."
 
@@ -98,7 +83,7 @@ crontab -l | grep -v 'app_start.sh' > cron_backup_filtered
 if cmp -s cron_backup_filtered cron_backup.bak; then
   echo "No cron jobs with 'app_start.sh' found."
 else
-  crontab cron_backup_filtered
+  sudo crontab cron_backup_filtered
   echo "Cron jobs containing 'app_start.sh' have been removed."
 fi
 
@@ -110,7 +95,7 @@ echo "Adding app_start.sh reboot cron job..."
 sudo chmod +x ./cicd-scripts/app_start.sh
 
 # Define the new cron job
-new_cron="@reboot at now + 1 min -f ${pwd}/cicd-scripts/app_start.sh"
+new_cron="@reboot at now + 1 min -f $(pwd)/cicd-scripts/app_start.sh"
 
 # Add the new cron job to the crontab if it's not already present
 (crontab -l | grep -v "$new_cron" ; echo "$new_cron") | crontab -
--- cicd-scripts/app_start.sh
+++ cicd-scripts/app_start.sh
@@ -2,12 +2,12 @@
 
 SCRAPYD_URL="http://127.0.0.1:6800/"
 SCRAPYDWEB_URL="http://127.0.0.1:5000/"
-CICD_SCRIPTS_BASE_DIR=$(pwd)
+SPIDER_URLS_API=https://staging.search.usa.gov/urls
 
 # Function to check if a URL is up and running
 function check_url() {
     local URL=$1
-    local MAX_ATTEMPTS=3
+    local MAX_ATTEMPTS="${2:-3}"
     local DELAY=5
     local attempt=1
 
@@ -26,18 +26,6 @@ function check_url() {
     return 1
 }
 
-# Function to combine current directory with subdirectory and return absolute path
-function get_abs_path() {
-    local base_dir="$CICD_SCRIPTS_BASE_DIR"
-    local sub_dir="$1"
-
-    if [[ "$sub_dir" == /* ]]; then
-        echo "$sub_dir"
-    else
-        echo "$base_dir/$sub_dir"
-    fi
-}
-
 # Function to check if required command exists
 function check_command() {
     if ! command -v "$1" &> /dev/null; then
@@ -50,27 +38,31 @@ check_command "scrapyd"
 check_command "scrapydweb"
 check_command "curl"
 
-scrapyd_dir=$(get_abs_path "../")
-scrapydweb_dir=$(get_abs_path "../search_gov_crawler")
-
 echo "Killing any existing scrapyd and scrapydweb services"
 sudo pkill -f "scrapydweb" 2>/dev/null
 sudo pkill -f "scrapyd" 2>/dev/null
+
+# Check search-gov /urls endpoint
+echo "Checking search-gov /urls api..."
+if check_url "$SPIDER_URLS_API"; then
+    echo "The /urls api is up and running at endpoint: $SPIDER_URLS_API"
+else
+    echo "Error: /urls failed failed at endpoint: $SPIDER_URLS_API"
+    exit 1
+fi
 
 echo "Running searchgov-spider application..."
 
 # Start scrapyd
 echo "Starting scrapyd service..."
-cd "$scrapyd_dir"
-sudo nohup scrapyd > /var/log/scrapyd.log 2>&1 &
+sudo bash -c 'nohup scrapyd > /var/log/scrapyd.log 2>&1 &'
 PID1=$!
 echo "Started scrapyd with PID $PID1"
 
 # Check if scrapyd is running
 if check_url "$SCRAPYD_URL"; then
     echo "The scrapyd service is running at $SCRAPYD_URL"
-    cd "$scrapydweb_dir"
-    sudo nohup scrapydweb > /var/log/scrapydweb.log 2>&1 &
+    sudo bash -c 'nohup cd ./search_gov_crawler && scrapydweb > /var/log/scrapydweb.log 2>&1 &'
     PID2=$!
     echo "Started scrapydweb with PID $PID2"
 
@@ -84,11 +76,6 @@ else
     echo "Error: scrapyd failed at $SCRAPYD_URL."
     exit 1
 fi
-
-# Add startup cron for this script:
-echo "
-export LATEST_SPIDER_CICD_DEPLOY_PATH=$(CICD_SCRIPTS_BASE_DIR)
-" | tee '/etc/profile.d/spider_env.sh' > /dev/null
 
 # Display the last few lines of logs
 echo -e "\n-- Last 10 lines of scrapyd.log:\n"
