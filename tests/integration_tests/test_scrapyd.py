415ed12b4cec921237986c4c787103565987de94
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2024-10-22 13:40:19 -0400

    SRCH-5569 increase wait for circle

--- tests/integration_tests/test_scrapyd.py
+++ tests/integration_tests/test_scrapyd.py
@@ -28,7 +28,7 @@ class TestScrapyd:
     @pytest.fixture(scope="class", name="scrapyd_process")
     def fixture_scrapyd_process(self, scrapyd_cwd):
         with subprocess.Popen(["scrapyd"], cwd=scrapyd_cwd) as scrapyd_process:
-            time.sleep(5)
+            time.sleep(30)
             yield scrapyd_process
             scrapyd_process.kill()
             Path(scrapyd_cwd / "twistd.pid").unlink(missing_ok=True)
