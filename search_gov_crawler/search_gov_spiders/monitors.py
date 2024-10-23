c3c823454f028a9b58f7e28ba3554a78c141ab71
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-10-23 15:23:57 -0400

    use settings variables

--- search_gov_crawler/search_gov_spiders/monitors.py
+++ search_gov_crawler/search_gov_spiders/monitors.py
@@ -4,8 +4,9 @@ from spidermon.contrib.monitors.mixins.stats import StatsMonitorMixin
 
 # SPIDERMON_UNWANTED_HTTP_CODES = "SPIDERMON_UNWANTED_HTTP_CODES"
 # SPIDERMON_UNWANTED_HTTP_CODES_MAX_COUNT = "SPIDERMON_UNWANTED_HTTP_CODES_MAX_COUNT"
-# SPIDERMON_MAX_EXECUTION_TIME = "SPIDERMON_MAX_EXECUTION_TIME"
-# SPIDERMON_ITEM_COUNT_INCREASE = "SPIDERMON_ITEM_COUNT_INCREASE"
+SPIDERMON_MAX_EXECUTION_TIME = "SPIDERMON_MAX_EXECUTION_TIME"
+SPIDERMON_ITEM_COUNT_INCREASE = "SPIDERMON_ITEM_COUNT_INCREASE"
+SPIDERMON_MIN_ITEMS = "SPIDERMON_MIN_ITEMS"
 
 @monitors.name('Item count monitor')
 class ItemCountMonitor(Monitor):
@@ -18,9 +19,11 @@ class ItemCountMonitor(Monitor):
 
     @monitors.name('Minimum number of items')
     def test_minimum_number_of_items(self):
+        crawler = self.data.get("crawler")
+
         item_extracted = getattr(
             self.data.stats, 'item_scraped_count', 0)
-        minimum_threshold = 10
+        minimum_threshold = crawler.settings.getint(SPIDERMON_MIN_ITEMS)
 
         msg = 'Extracted less than {} items'.format(
             minimum_threshold)
@@ -158,9 +161,10 @@ class PeriodicItemCountMonitor(Monitor):
 
     @monitors.name('Check SPIDERMON_ITEM_COUNT_INCREASE number of items returned in SPIDERMON_TIME_INTERVAL seconds')
     def test_number_of_items_in_interval(self):
+        crawler = self.data.get("crawler")
         item_extracted = getattr(
             self.data.stats, 'item_scraped_count', 0)
-        minimum_threshold = 10
+        minimum_threshold = crawler.settings.getint(SPIDERMON_ITEM_COUNT_INCREASE)
 
         msg = 'Extracted less than {} items'.format(
             minimum_threshold)
@@ -175,10 +179,11 @@ class PeriodicExecutionTimeMonitor(Monitor, StatsMonitorMixin):
     You can configure the maximum runtime (in seconds) using
     ``SPIDERMON_MAX_EXECUTION_TIME`` as a project setting or spider attribute."""
         
-    # max_execution_time is how long we want it to be running max whereas SPIDERMON_TIME_INTERVAL in settings.py is how often we check that it's running
+    # SPIDERMON_MAX_EXECUTION_TIME is how long we want it to be running max whereas SPIDERMON_TIME_INTERVAL in settings.py is how often we check that it's running
     @monitors.name("Maximum execution time reached")
     def test_execution_time(self):
-        max_execution_time = 1
+        crawler = self.data.get("crawler")
+        max_execution_time = crawler.settings.getint(SPIDERMON_MAX_EXECUTION_TIME)
         if not max_execution_time:
             return
 
