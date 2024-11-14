6ce786a7d32a88027ade2ba86671c1fccfebc432
Isabel Laurenceau <108532577+IsabelLaurenceau@users.noreply.github.com>
2024-11-14 09:09:00 -0500

    Update monitors.py

--- search_gov_crawler/search_gov_spiders/monitors.py
+++ search_gov_crawler/search_gov_spiders/monitors.py
@@ -1,29 +1,15 @@
-import datetime
-import json
-import copy
 import os
 from pathlib import Path
-from spidermon import Monitor, MonitorSuite, monitors
-from spidermon.contrib.scrapy.monitors.monitors import ItemCountMonitor, UnwantedHTTPCodesMonitor, PeriodicItemCountMonitor, PeriodicExecutionTimeMonitor
-from spidermon.contrib.actions.reports.files import CreateFileReport
+from spidermon import MonitorSuite
 from spidermon.contrib.actions.email.smtp import SendSmtpEmail
+from spidermon.contrib.actions.reports.files import CreateFileReport
+from spidermon.contrib.scrapy.monitors.monitors import ItemCountMonitor, UnwantedHTTPCodesMonitor, PeriodicItemCountMonitor, PeriodicExecutionTimeMonitor
 
 SPIDERMON_UNWANTED_HTTP_CODES = "SPIDERMON_UNWANTED_HTTP_CODES"
 SPIDERMON_UNWANTED_HTTP_CODES_MAX_COUNT = "SPIDERMON_UNWANTED_HTTP_CODES_MAX_COUNT"
 SPIDERMON_MAX_EXECUTION_TIME = "SPIDERMON_MAX_EXECUTION_TIME"
 SPIDERMON_ITEM_COUNT_INCREASE = "SPIDERMON_ITEM_COUNT_INCREASE"
 SPIDERMON_MIN_ITEMS = "SPIDERMON_MIN_ITEMS"
-
-def getdictorlist(crawler, name, default=None):
-    value = crawler.settings.get(name, default)
-    if value is None:
-        return {}
-    if isinstance(value, str):
-        try:
-            return json.loads(value, object_pairs_hook=OrderedDict)
-        except ValueError:
-            return value.split(",")
-    return copy.deepcopy(value)
 
 class CreateCustomFileReport(CreateFileReport):
     template_paths = [Path(__file__).parent / "actions"]
