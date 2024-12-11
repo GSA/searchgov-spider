b4779fb6c910090a78fcafe374e2843605ad30ee
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-12-11 16:39:16 -0500

    Update pipelines.py
    Since we are using multiple spiders (processes) need to use a system file lock. Will update to share the line number to do the pagination tomorrow. But running this should return all urls without overwrites

--- search_gov_crawler/search_gov_spiders/pipelines.py
+++ search_gov_crawler/search_gov_spiders/pipelines.py
@@ -7,6 +7,7 @@ import os
 import threading
 from pathlib import Path
 from scrapy.exceptions import DropItem
+import fcntl
 
 class SearchGovSpidersPipeline:
     """
@@ -19,16 +20,20 @@ class SearchGovSpidersPipeline:
         self.file_number = 1
         self.parent_file_path = Path(__file__).parent.parent.resolve()
         self.base_path_name = str(self.parent_file_path / "output/all-links.csv")
-        self.short_file = open(self.base_path_name, "w", encoding="utf-8")
+        self.short_file = open(self.base_path_name, "a", encoding="utf-8")
         self.max_file_size = 3900
-        self.paginate = True
+        self.paginate = False
         self.lock = threading.Lock()
+        self.line_number = 0
 
     def process_item(self, item, _spider):
         """Checks that the file is not at max size.
         Adds it to the file if less, or creates a new file if too large."""
         with self.lock: 
+            fcntl.flock(self.short_file.fileno(), fcntl.LOCK_EX)
             line = item["url"]
+            self.line_number += 1
+            print(f'{self.line_number}) The line is: {line}')
             self.current_file_size += 1
             next_file_size = self.current_file_size + len(line)
             if self.paginate and next_file_size > self.max_file_size:
@@ -38,10 +43,11 @@ class SearchGovSpidersPipeline:
                 self.file_number = self.file_number + 1
                 self.short_file = open(self.base_path_name, "w", encoding="utf-8")
                 self.current_file_size = 0
-
             self.short_file.write(line)
+            print(f'{self.line_number}) WRITTEN: The line is: {line}')
             self.short_file.write("\n")
             self.current_file_size = self.current_file_size + len(line)
+            fcntl.flock(self.short_file.fileno(), fcntl.LOCK_UN)
             return item
 
 
