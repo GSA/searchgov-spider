d6785b1fc6b9a987dd74bf24ee61007ba0c3c315
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-12-11 09:05:38 -0500

    At thread lock to pipeline

--- search_gov_crawler/search_gov_spiders/pipelines.py
+++ search_gov_crawler/search_gov_spiders/pipelines.py
@@ -4,10 +4,9 @@ See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 """
 
 import os
+import threading
 from pathlib import Path
-
 from scrapy.exceptions import DropItem
-
 
 class SearchGovSpidersPipeline:
     """
@@ -23,27 +22,27 @@ class SearchGovSpidersPipeline:
         self.short_file = open(self.base_path_name, "w", encoding="utf-8")
         self.max_file_size = 3900
         self.paginate = True
+        self.lock = threading.Lock()
 
     def process_item(self, item, _spider):
         """Checks that the file is not at max size.
         Adds it to the file if less, or creates a new file if too large."""
+        with self.lock: 
+            line = item["url"]
+            self.current_file_size += 1
+            next_file_size = self.current_file_size + len(line)
+            if self.paginate and next_file_size > self.max_file_size:
+                self.short_file.close()
+                new_name = str(self.parent_file_path / f"output/all-links{self.file_number}.csv")
+                os.rename(self.base_path_name, new_name)
+                self.file_number = self.file_number + 1
+                self.short_file = open(self.base_path_name, "w", encoding="utf-8")
+                self.current_file_size = 0
 
-        line = item["url"]
-        self.current_file_size += 1
-        next_file_size = self.current_file_size + len(line)
-        if self.paginate and next_file_size > self.max_file_size:
-            self.short_file.close()
-            new_name = str(self.parent_file_path / f"output/all-links{self.file_number}.csv")
-            os.rename(self.base_path_name, new_name)
-            self.file_number = self.file_number + 1
-            self.short_file = open(self.base_path_name, "w", encoding="utf-8")
-            self.current_file_size = 0
-
-        self.short_file.write(line)
-        self.short_file.write("\n")
-        self.current_file_size = self.current_file_size + len(line)
-
-        return item
+            self.short_file.write(line)
+            self.short_file.write("\n")
+            self.current_file_size = self.current_file_size + len(line)
+            return item
 
 
 class DeDeuplicatorPipeline:
