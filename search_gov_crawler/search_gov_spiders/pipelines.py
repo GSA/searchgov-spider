bdb9b4be626f02c7eff8cdd4228e1ef620c1a303
Isabel <108532577+IsabelLaurenceau@users.noreply.github.com>
2024-12-12 15:20:54 -0500

    SRCH-5748-Update-Spider-FEEDS-Setting (#60)

--- search_gov_crawler/search_gov_spiders/pipelines.py
+++ search_gov_crawler/search_gov_spiders/pipelines.py
@@ -5,9 +5,7 @@ See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 import os
 from pathlib import Path
-
 from scrapy.exceptions import DropItem
-
 
 class SearchGovSpidersPipeline:
     """
@@ -19,30 +17,29 @@ class SearchGovSpidersPipeline:
         self.current_file_size = 0
         self.file_number = 1
         self.parent_file_path = Path(__file__).parent.parent.resolve()
-        self.base_path_name = str(self.parent_file_path / "output/all-links.csv")
-        self.short_file = open(self.base_path_name, "w", encoding="utf-8")
-        self.max_file_size = 3900
+        self.base_path_name = str(self.parent_file_path / f"output/all-links-p{os.getpid()}.csv")
+        self.short_file = open(self.base_path_name, "a", encoding="utf-8")
+        self.max_file_size = 39000000 #3.9MB max
         self.paginate = True
 
     def process_item(self, item, _spider):
         """Checks that the file is not at max size.
         Adds it to the file if less, or creates a new file if too large."""
-
         line = item["url"]
         self.current_file_size += 1
+        file_stats = os.stat(self.base_path_name)
+        self.current_file_size += file_stats.st_size
         next_file_size = self.current_file_size + len(line)
         if self.paginate and next_file_size > self.max_file_size:
             self.short_file.close()
-            new_name = str(self.parent_file_path / f"output/all-links{self.file_number}.csv")
+            new_name = str(self.parent_file_path / f"output/all-links-p{os.getpid()}-{self.file_number}.csv")
             os.rename(self.base_path_name, new_name)
             self.file_number = self.file_number + 1
             self.short_file = open(self.base_path_name, "w", encoding="utf-8")
             self.current_file_size = 0
-
         self.short_file.write(line)
         self.short_file.write("\n")
         self.current_file_size = self.current_file_size + len(line)
-
         return item
 
 
