58d5495eda939696935a59d7de75df06cfa47458
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2024-12-12 12:22:27 -0500

    ...

--- cicd-scripts/app_start.sh
+++ cicd-scripts/app_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # PUBLIC
-RUN_WITH_UI=false
+SPIDER_RUN_WITH_UI=false
 
-if $RUN_WITH_UI ; then
+if $SPIDER_RUN_WITH_UI ; then
     sudo chmod +x ./cicd-scripts/helpers/run_with_ui.sh
     source ./cicd-scripts/helpers/run_with_ui.sh
 else
