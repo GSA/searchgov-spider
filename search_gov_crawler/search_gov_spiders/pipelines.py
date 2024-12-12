df433c75f2bf2120773c0f458da3814addfd7693
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-12-12 07:40:32 -0500

    Add in pagination

--- search_gov_crawler/search_gov_spiders/pipelines.py
+++ search_gov_crawler/search_gov_spiders/pipelines.py
@@ -4,7 +4,6 @@ See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 """
 
 import os
-import threading
 from pathlib import Path
 from scrapy.exceptions import DropItem
 import fcntl
@@ -19,36 +18,32 @@ class SearchGovSpidersPipeline:
         self.current_file_size = 0
         self.file_number = 1
         self.parent_file_path = Path(__file__).parent.parent.resolve()
-        self.base_path_name = str(self.parent_file_path / "output/all-links.csv")
+        self.base_path_name = str(self.parent_file_path / f"output/all-links-p{os.getpid()}.csv")
         self.short_file = open(self.base_path_name, "a", encoding="utf-8")
-        self.max_file_size = 3900
-        self.paginate = False
-        self.lock = threading.Lock()
-        self.line_number = 0
+        self.max_file_size = 39000000 #3.9MB max
+        self.paginate = True
 
     def process_item(self, item, _spider):
         """Checks that the file is not at max size.
         Adds it to the file if less, or creates a new file if too large."""
-        with self.lock: 
-            fcntl.flock(self.short_file.fileno(), fcntl.LOCK_EX)
-            line = item["url"]
-            self.line_number += 1
-            print(f'{self.line_number}) The line is: {line}')
-            self.current_file_size += 1
-            next_file_size = self.current_file_size + len(line)
-            if self.paginate and next_file_size > self.max_file_size:
-                self.short_file.close()
-                new_name = str(self.parent_file_path / f"output/all-links{self.file_number}.csv")
-                os.rename(self.base_path_name, new_name)
-                self.file_number = self.file_number + 1
-                self.short_file = open(self.base_path_name, "w", encoding="utf-8")
-                self.current_file_size = 0
-            self.short_file.write(line)
-            print(f'{self.line_number}) WRITTEN: The line is: {line}')
-            self.short_file.write("\n")
-            self.current_file_size = self.current_file_size + len(line)
-            fcntl.flock(self.short_file.fileno(), fcntl.LOCK_UN)
-            return item
+        fcntl.flock(self.short_file.fileno(), fcntl.LOCK_EX)
+        line = item["url"]
+        self.current_file_size += 1
+        file_stats = os.stat(self.base_path_name)
+        self.current_file_size += file_stats.st_size
+        next_file_size = self.current_file_size + len(line)
+        if self.paginate and next_file_size > self.max_file_size:
+            self.short_file.close()
+            new_name = str(self.parent_file_path / f"output/all-links-p{os.getpid()}-{self.file_number}.csv")
+            os.rename(self.base_path_name, new_name)
+            self.file_number = self.file_number + 1
+            self.short_file = open(self.base_path_name, "w", encoding="utf-8")
+            self.current_file_size = 0
+        self.short_file.write(line)
+        self.short_file.write("\n")
+        self.current_file_size = self.current_file_size + len(line)
+        fcntl.flock(self.short_file.fileno(), fcntl.LOCK_UN)
+        return item
 
 
 class DeDeuplicatorPipeline:
