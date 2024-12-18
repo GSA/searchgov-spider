366a04475d969f796417bb5df1c01fe8d45d5c28
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-12-18 07:38:30 -0500

    Python working Scrapy not

--- startcommand.sh
+++ startcommand.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd search_gov_crawler
-bash -c "scrapy crawl domain_spider -a allowed_domains="quotes.toscrape.com/tag/" -a start_urls="https://quotes.toscrape.com""
+ls
+python -c "print('Hello World')"
+python -c 'scrapy crawl domain_spider -a allowed_domains="quotes.toscrape.com/tag/" -a start_urls="https://quotes.toscrape.com"'
