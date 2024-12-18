3b188d91f5e2ac0e8d391b28564aea2f19d6b2f5
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-12-18 15:34:51 -0500

    python path working

--- startcommand.sh
+++ startcommand.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+echo "START START COMMAND"
 # Define the current directory
 # CURRENT_DIR=$(pwd)
-ls -a
+# ls -a
 # echo "Your current directory is $CURRENT_DIR"
-
 cd search_gov_crawler
-ls
+# ls
 which python
 echo $PYTHONPATH
 python -c "print('Hello World')"
 python benchmark.py -f ./test_parallel_domain.json
+echo "END START COMMAND"
