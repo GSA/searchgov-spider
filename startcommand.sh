bdf53fa2847a685260956e012dd46df56cc4c368
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-12-19 09:23:00 -0500

    with egress changes changes, works

--- startcommand.sh
+++ startcommand.sh
@@ -9,5 +9,7 @@ cd search_gov_crawler
 which python
 echo $PYTHONPATH
 python -c "print('Hello World')"
-python benchmark.py -f ./test_parallel_domain.json
+# python benchmark.py -f ./test_parallel_domain.json
+python benchmark.py -d "quotes.toscrape.com" -u "https://quotes.toscrape.com"
+# python -m http.server
 echo "END START COMMAND"
