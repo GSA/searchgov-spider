9ce68bf004159558f8f00c73c6d47d3b05a2d1bd
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2024-12-31 04:56:36 -0500

    ...

--- cicd-scripts/helpers/run_without_ui.sh
+++ cicd-scripts/helpers/run_without_ui.sh
@@ -3,6 +3,9 @@
 # Run the script in the background using the virtual environment
 chmod +x ./search_gov_crawler/scrapy_scheduler.py
 
-sudo nohup bash -c "source ./venv/bin/activate && ./venv/bin/python ./search_gov_crawler/scrapy_scheduler.py" > /var/log/scrapy_scheduler.log 2>&1 &
+sudo touch /var/log/scrapy_scheduler.log
+sudo chown -R $(whoami) /var/log/scrapy_scheduler.log
+
+nohup bash -c "source ./venv/bin/activate && ./venv/bin/python ./search_gov_crawler/scrapy_scheduler.py" | tee -a /var/log/scrapy_scheduler.log &
 
 echo "Running no UI vesrion of searchgov-spider..."
