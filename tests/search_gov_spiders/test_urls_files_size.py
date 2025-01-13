9ec8889d086f932a09de6d20f56f453e4f5e7b53
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2025-01-13 13:20:13 -0500

    SRCH-5795 lower limit for rotating files and batching POST requests

--- tests/search_gov_spiders/test_urls_files_size.py
+++ tests/search_gov_spiders/test_urls_files_size.py
@@ -24,6 +24,14 @@ def fixture_sample_item() -> SearchGovSpidersItem:
     return item
 
 
+@pytest.fixture(name="sample_item_long")
+def fixture_sample_item_long() -> SearchGovSpidersItem:
+    """Fixture for a longer sample item to make triggering rotate a bit easier"""
+    item = SearchGovSpidersItem()
+    item["url"] = f"http://example.com/this/is/a/{'really/' * 141}long/url"  # len 1024
+    return item
+
+
 @pytest.fixture(name="mock_open")
 def fixture_mock_open(mocker):
     return mocker.patch("builtins.open", mocker.mock_open())
@@ -68,7 +76,7 @@ def test_post_to_api(pipeline_with_api, sample_item, sample_spider, mocker):
     mocker.patch.object(
         SearchGovSpidersPipeline,
         "_batch_size",
-        return_value=SearchGovSpidersPipeline.MAX_FILE_SIZE_BYTES,
+        return_value=SearchGovSpidersPipeline.MAX_URL_BATCH_SIZE_BYTES,
     )
     pipeline_with_api.process_item(sample_item, sample_spider)
 
@@ -82,7 +90,7 @@ def test_rotate_file(pipeline_no_api, mock_open, sample_item, mocker):
     mocker.patch.object(
         SearchGovSpidersPipeline,
         "_file_size",
-        return_value=SearchGovSpidersPipeline.MAX_FILE_SIZE_BYTES,
+        return_value=SearchGovSpidersPipeline.MAX_URL_BATCH_SIZE_BYTES,
     )
     pipeline_no_api.process_item(sample_item, None)
 
@@ -90,6 +98,24 @@ def test_rotate_file(pipeline_no_api, mock_open, sample_item, mocker):
     mock_open.assert_called_with(pipeline_no_api.file_path, "a", encoding="utf-8")
     mock_open().close.assert_called()
     mock_rename.assert_called_once()
+
+
+def test_post_to_api_size_limit(pipeline_with_api, mocker, sample_spider, sample_item_long):
+    """Validate size limit checking with API calls enabled"""
+    mock_post = mocker.patch("requests.post")
+
+    for _ in range(200):
+        pipeline_with_api.process_item(sample_item_long, sample_spider)
+
+    pipeline_with_api.close_spider(sample_spider)
+    # Ensure POST request was made
+    calls = [
+        mocker.call("http://mockapi.com", json=mocker.ANY),
+        mocker.call().raise_for_status(),
+        mocker.call("http://mockapi.com", json=mocker.ANY),
+        mocker.call().raise_for_status(),
+    ]
+    mock_post.assert_has_calls(calls)
 
 
 def test_post_urls_on_spider_close(pipeline_with_api, sample_spider, mocker):
